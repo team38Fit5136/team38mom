6fbae2e4ff8c82e9ed59c14f475c7bc6f25200b1
bmit0010 <bmit0010@student.monash.edu>
2020-04-29 01:30:16 +1000

    added base for implementation spring boot backend code

---
+++ marsmissionbackend/src/main/java/com/marsmission/team38/MarsmissionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.marsmission.team38;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsmissionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsmissionbackendApplication.class, args);
+	}
+
+}
