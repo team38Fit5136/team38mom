775b0f4707430cf2c18aac140d98b0707561e3f2
bmit0010 <bmit0010@student.monash.edu>
2020-04-29 16:26:33 +1000

    added swagger for documentation

--- marsmissionbackend/MOMUserModelMS/src/main/java/com/marsmission/team38/MomUserModelMsApplication.java
+++ marsmissionbackend/MOMUserModelMS/src/main/java/com/marsmission/team38/MomUserModelMsApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan("com.marsmission.*")
 public class MomUserModelMsApplication {
 
