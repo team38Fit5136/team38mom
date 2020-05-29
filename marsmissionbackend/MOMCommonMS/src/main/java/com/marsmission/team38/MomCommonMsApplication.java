4999662efd7ef5ee0ed4b2f3ea375c31345b1ce3
bakul6193 <63577954+bakul6193@users.noreply.github.com>
2020-05-29 14:56:36 +1000

    Update MomCommonMsApplication.java

--- marsmissionbackend/MOMCommonMS/src/main/java/com/marsmission/team38/MomCommonMsApplication.java
+++ marsmissionbackend/MOMCommonMS/src/main/java/com/marsmission/team38/MomCommonMsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan("com.marsmission.*")
 public class MomCommonMsApplication {
 
 	public static void main(String[] args) {
