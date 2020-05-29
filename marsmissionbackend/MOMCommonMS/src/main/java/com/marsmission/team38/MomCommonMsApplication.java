802b7a90595cd315ebbcf2d5c39229892e150c3a
bmit0010 <bmit0010@student.monash.edu>
2020-05-29 14:51:44 +1000

    microservice added for common methods

--- marsmissionbackend/MOMCommonMS/src/main/java/com/marsmission/team38/MomCommonMsApplication.java
+++ marsmissionbackend/MOMCommonMS/src/main/java/com/marsmission/team38/MomCommonMsApplication.java
@@ -4,17 +4,12 @@ import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@ComponentScan("com.marsmission.*")
-public class MomUserModelMsApplication {
+public class MomCommonMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(MomUserModelMsApplication.class);
+		SpringApplication springApplication = new SpringApplication(MomCommonMsApplication.class);
 		Properties properties = new Properties();
 		springApplication.setDefaultProperties(properties);
 		springApplication.run(args);
