4c79a06852e158ab9c05aabaf11674301d977a60
bmit0010 <bmit0010@student.monash.edu>
2020-06-06 22:26:52 +1000

    create mission updating

--- marsmissionbackend/MOMMissionModelMS/src/main/java/com/marsmission/team38/missionController/MissionController.java
+++ marsmissionbackend/MOMMissionModelMS/src/main/java/com/marsmission/team38/missionController/MissionController.java
@@ -1,22 +1,25 @@
 package com.marsmission.team38.missionController;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marsmission.team38.missionService.MissionServcie;
 
 /**
@@ -25,39 +28,64 @@ import com.marsmission.team38.missionService.MissionServcie;
  */
 @RestController
 @RequestMapping("/mom/mission")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class MissionController {
 
-	//logger variable to print logs
+	// logger variable to print logs
 	private Log logger = LogFactory.getLog(this.getClass());
+	ObjectMapper mapper = new ObjectMapper();
 
 	@Autowired
 	private MissionServcie missionService;
 
-	//api call for adding mission
-	@PostMapping("")
-	public Map<String, Serializable> addMission(@RequestParam("file") MultipartFile[] files,
-			@RequestBody Map<String, ?> props) throws SQLException {
-		logger.info("in addMission"+files+"            "+props);
+	// api call for adding mission
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public Map<String, Serializable> addMission(
+			@RequestParam(value = "cargoForJourney", required = false,defaultValue = "null") MultipartFile cargoForJourney,
+			@RequestParam(value = "cargoForMission", required = false,defaultValue = "null") MultipartFile cargoForMission,
+			@RequestParam(value = "cargoForOtherMission", required = false,defaultValue = "null") MultipartFile cargoForOtherMission,
+			@RequestParam("props") String props) throws Exception {
+		logger.info("in addMission" + cargoForJourney + "            " + props);
+		JsonNode actualObj = mapper.readTree(props);
+//		
+		String path = "/home/bakul/Music/";
+
+		File cargoForJourneyFile = new File(cargoForJourney.getOriginalFilename());
+		FileReader cargoForJourneyFileReader = new FileReader(path+cargoForJourneyFile);
+
+		File cargoForMissionFile = new File(cargoForMission.getOriginalFilename());
+		FileReader cargoForMissionFileReader = new FileReader(path+cargoForMissionFile);
+
+		File cargoForOtherMissionFile = new File(cargoForOtherMission.getOriginalFilename());
+		FileReader cargoForOtherMissionReader = new FileReader(path+cargoForOtherMissionFile);
+
+		Map<String, Object> body = mapper.convertValue(actualObj, new TypeReference<Map<String, Object>>() {
+		});
+
+		logger.info(body);
+
+		missionService.addMissionService(body, cargoForJourneyFileReader, cargoForMissionFileReader, cargoForOtherMissionReader);
 		return null;
 //		return missionService.addMissionService(props);
 	}
 
-	//api call for getting mission
+	// api call for getting mission
 	@GetMapping("")
 	public Map<String, Serializable> getMissionDetails(
-			@RequestParam(value = "missionID", required = true, defaultValue = "null") String missionID) throws Exception {
+			@RequestParam(value = "missionID", required = true, defaultValue = "null") String missionID)
+			throws Exception {
 		logger.info("in get getMissionDetails" + missionID);
 		return missionService.getMissionDetailsService(missionID);
 	}
 
-	//api call for updating Mission
+	// api call for updating Mission
 	@PutMapping("")
 	public Map<String, Serializable> updateMissionDetails(
-			@RequestParam(value = "missionID", required = true) String missionID,
-			@RequestBody Map<String, ?> props) {
+			@RequestParam(value = "missionID", required = true) String missionID, @RequestBody Map<String, ?> props) {
 		logger.info("missionID" + missionID);
 		return missionService.updateMissionDetailsService(missionID, props);
 	}
-
+	
+	
+	
 }
--- marsmissionbackend/MOMMissionModelMS/src/main/java/com/marsmission/team38/missionService/MissionServcie.java
+++ marsmissionbackend/MOMMissionModelMS/src/main/java/com/marsmission/team38/missionService/MissionServcie.java
@@ -1,36 +1,35 @@
 package com.marsmission.team38.missionService;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.stereotype.Service;
 
+import com.csvreader.CsvReader;
 import com.marsmission.team38.missionDAO.MissionDAO;
 
 @Service
 public class MissionServcie {
 
-	//logger variable to print logs
+	// logger variable to print logs
 	private Log logger = LogFactory.getLog(this.getClass());
 	@Autowired
 	private MissionDAO missionDAO;
 
-	//variables whose values will be populated based on json request
-	@Value("${mission.mandatory.cargoID}")
-	boolean cargoIDMandatory;
+	// variables checks form property files
 
 	@Value("${mission.mandatory.coordinatorID}")
 	boolean coordinatorIDMandatory;
-
-	@Value("${mission.mandatory.countryAllowed}")
-	boolean countryAllowedMandatory;
 
 	@Value("${mission.mandatory.countryOrigin}")
 	boolean countryOriginMandatory;
@@ -38,12 +37,8 @@ public class MissionServcie {
 	@Value("${mission.mandatory.duration}")
 	boolean durationMandatory;
 
-	
 	@Value("${mission.mandatory.launchDate}")
 	boolean launchDateMandatory;
-
-	@Value("${mission.mandatory.locationID}")
-	boolean locationIDMandatory;
 
 	@Value("${mission.mandatory.missionDetails}")
 	boolean missionDetailsMandatory;
@@ -51,147 +46,167 @@ public class MissionServcie {
 	@Value("${mission.mandatory.missionName}")
 	boolean missionNameMandatory;
 
-	@Value("${mission.mandatory.shuttleID}")
-	boolean shuttleIDMandatory;
-
 	@Value("${mission.mandatory.statusID}")
 	boolean statusIDMandatory;
 
-	
 	// Method for creating mission
-	public Map<String, Serializable> addMissionService(Map<String, ?> props) {
 
+	public Map<String, Serializable> addMissionService(Map<String, Object> body, FileReader cargoForJourneyFileReader,
+			FileReader cargoForMissionFileReader, FileReader cargoForOtherMissionFileReader) throws IOException {
+	
 		Map<String, Serializable> result = new HashMap<>();
-		logger.info("Mission" + props);
+		List<String> headers = new ArrayList<String>();
 
-		long missionID = 0;
-		Integer cargoID = (int) (props.containsKey("cargoID") ? props.get("cargoID") : 0);
-		Integer coordinatorID = (int) (props.containsKey("coordinatorID") ? props.get("coordinatorID") : 0);
-		Integer locationID = (int) (props.containsKey("locationID") ? props.get("locationID") : 0);
-		Integer shuttleID = (int) (props.containsKey("shuttleID") ? props.get("shuttleID") : 0);
-		Integer statusID = (int) (props.containsKey("statusID") ? props.get("statusID") : 0);
-		Integer countryOrigin = (int) (props.containsKey("countryOrigin") ? props.get("countryOrigin") : 0);
+		logger.info("Mission" + body);
+		CsvReader cargoForJourney = new CsvReader(cargoForJourneyFileReader);
 
-		String countryAllowed = (String) (props.containsKey("countryAllowed") ? props.get("countryAllowed") : null);// Json
-		String duration = (String) (props.containsKey("duration") ? props.get("duration") : null);
-		String launchDate = (String) (props.containsKey("launchDate") ? props.get("launchDate") : null);
-		String missionDetails = (String) (props.containsKey("missionDetails") ? props.get("missionDetails") : null);
-		String missionName = (String) (props.containsKey("missionName") ? props.get("missionName") : null);
+		while (cargoForJourney.readRecord()) {
+			logger.info("inside loop");
+			Map<String, Serializable> cargoForJourneyMap = new HashMap<String, Serializable>();
 
-		if (cargoIDMandatory) {
-			if (cargoID == 0) {
-				logger.warn("cargoID is mandatory for the mission");
-				result.put("responseMsg", "cargoID is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
+			int columnCount = cargoForJourney.getColumnCount();
 
-		if (coordinatorIDMandatory) {
-			if (coordinatorID == 0) {
-				logger.warn("coordinatorID is mandatory for the mission");
-				result.put("responseMsg", "coordinatorID is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
+			for (int i = 0; i < columnCount; i++) {
+				logger.info("products.get(i):" + cargoForJourney.get(i));
+				headers.add(cargoForJourney.get(i));
 			}
 		}
-		if (locationIDMandatory) {
-			if (locationID == 0) {
-				logger.warn("Location ID is mandatory for the mission");
-				result.put("responseMsg", "Location ID is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (shuttleIDMandatory) {
-			if (shuttleID == 0) {
-				logger.warn("Shuttle ID is mandatory for the mission");
-				result.put("responseMsg", "Shuttle ID is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (statusIDMandatory) {
-			if (statusID == 0) {
-				logger.warn("Status ID is mandatory for the mission");
-				result.put("responseMsg", "Status ID is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (countryOriginMandatory) {
-			if (countryOrigin == 0) {
-				logger.warn("Country of Origin is mandatory for the mission");
-				result.put("responseMsg", "Country of Origin is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-
-		if (countryAllowedMandatory) {
-			if (countryAllowed == null || (countryAllowed.toString().equalsIgnoreCase(""))) {
-				logger.warn("Country Allowed is mandatory for the mission");
-				result.put("responseMsg", "Country Allowed is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (durationMandatory) {
-			if (duration == null || (duration.toString().equalsIgnoreCase(""))) {
-				logger.warn("Duration is mandatory for the mission");
-				result.put("responseMsg", "Duration is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (launchDateMandatory) {
-			if (launchDate == null || (launchDate.toString().equalsIgnoreCase(""))) {
-				logger.warn("Launch Date is mandatory for the mission");
-				result.put("responseMsg", "Launch Date is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		if (missionDetailsMandatory) {
-			if (missionDetails == null || (missionDetails.toString().equalsIgnoreCase(""))) {
-				logger.warn("Mission Details is mandatory for the mission");
-				result.put("responseMsg", "Mission Details is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-
-		}
-		if (missionNameMandatory) {
-			if (missionName == null || (missionName.toString().equalsIgnoreCase(""))) {
-				logger.warn("Mission Name is mandatory for the mission");
-				result.put("responseMsg", "Mission Name is mandatory for the mission");
-				result.put("status", "failed");
-				return result;
-			}
-		}
-		try {
-			//adding mission and returning mission id
-			missionID = missionDAO.addMissionDAO(props);
-			if (missionID != 0) {
-				logger.info("added successfully");
-				result.put("missionID ", missionID);
-				result.put("status", "success");
-				result.put("responseMsg", "successfully inserted");
-			} else {
-				logger.error("Enter Detials is not correct ");
-				result.put("status", "failed");
-				result.put("responseMsg", "Enter Details is not correct ");
-			}
-		} catch (DuplicateKeyException e) {
-			logger.error("Duplciate key error" + e);
-			result.put("status", "failed");
-			result.put("responseMsg", "Duplicate entry already present");
-		} catch (UncategorizedSQLException e) {
-			logger.error("Uncategorized  error" + e);
-			result.put("status", "failed");
-			result.put("responseMsg", "Enter data is not correct");
-		}
-		return result;
+//
+//		int missionID = 0;
+//		int resp = 0;
+//		Integer coordinatorID = (int) (body.containsKey("coordinatorID") ? body.get("coordinatorID") : 0);
+//		Integer locationID = (int) (body.containsKey("locationID") ? body.get("locationID") : 0);
+//		Integer statusID = (int) (body.containsKey("statusID") ? body.get("statusID") : 0);
+//		Integer countryOrigin = (int) (body.containsKey("countryOrigin") ? body.get("countryOrigin") : 0);
+//
+//		List<String> countryAllowed = (List<String>) (body.containsKey("countryAllowed") ? body.get("countryAllowed")
+//				: null);// array
+//		List<String> jobIDs = (List<String>) (body.containsKey("jobID") ? body.get("jobID") : null);// array
+//
+//		Integer duration = (Integer) (body.containsKey("duration") ? body.get("duration") : null);
+//		String launchDate = (String) (body.containsKey("launchDate") ? body.get("launchDate") : null);
+//		String missionDetails = (String) (body.containsKey("missionDetails") ? body.get("missionDetails") : null);
+//		String missionName = (String) (body.containsKey("missionName") ? body.get("missionName") : null);
+//
+//		if (coordinatorIDMandatory) {
+//			if (coordinatorID == 0) {
+//				logger.warn("coordinatorID is mandatory for the mission");
+//				result.put("responseMsg", "coordinatorID is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//		if (statusIDMandatory) {
+//			if (statusID == 0) {
+//				logger.warn("Status ID is mandatory for the mission");
+//				result.put("responseMsg", "Status ID is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//		if (countryOriginMandatory) {
+//			if (countryOrigin == 0) {
+//				logger.warn("Country of Origin is mandatory for the mission");
+//				result.put("responseMsg", "Country of Origin is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//
+//		if (durationMandatory) {
+//			if (duration == null || (duration.toString().equalsIgnoreCase(""))) {
+//				logger.warn("Duration is mandatory for the mission");
+//				result.put("responseMsg", "Duration is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//		if (launchDateMandatory) {
+//			if (launchDate == null || (launchDate.toString().equalsIgnoreCase(""))) {
+//				logger.warn("Launch Date is mandatory for the mission");
+//				result.put("responseMsg", "Launch Date is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//		if (missionDetailsMandatory) {
+//			if (missionDetails == null || (missionDetails.toString().equalsIgnoreCase(""))) {
+//				logger.warn("Mission Details is mandatory for the mission");
+//				result.put("responseMsg", "Mission Details is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//
+//		}
+//		if (missionNameMandatory) {
+//			if (missionName == null || (missionName.toString().equalsIgnoreCase(""))) {
+//				logger.warn("Mission Name is mandatory for the mission");
+//				result.put("responseMsg", "Mission Name is mandatory for the mission");
+//				result.put("status", "failed");
+//				return result;
+//			}
+//		}
+//		try {
+//			// adding mission and returning mission id
+//			missionID = missionDAO.addMissionDAO(coordinatorID, statusID, countryOrigin, duration, launchDate,
+//					missionDetails, missionName);
+//			if (missionID != 0) {
+//				logger.info("added successfully");
+//				if (countryAllowed != null && !(countryAllowed.isEmpty())) {
+//					resp = missionDAO.addAllowedCountry(missionID, countryAllowed);
+//					resp = 0;
+//				} else if (resp == 0) {
+//					missionDAO.deleteMissionDAO(missionID);
+//					logger.error("Enter Detials is not correct  allowed country ");
+//					result.put("status", "failed");
+//					result.put("responseMsg", "Enter Detials is not correct  allowed country ");
+//					return result;
+//				}
+//				if (locationID != null && !(locationID != 0)) {
+//					resp = missionDAO.addMissionRelatedLocation(missionID, locationID);
+//					resp = 0;
+//				} else if (resp == 0) {
+//					missionDAO.deleteAllowedCountry(missionID);
+//					missionDAO.deleteMissionDAO(missionID);
+//					logger.error("Enter Detials is not correct  location");
+//					result.put("status", "failed");
+//					result.put("responseMsg", "Enter Detials is not correct location");
+//					return result;
+//				}
+//				if (jobIDs != null && !(jobIDs.isEmpty())) {
+//					resp = missionDAO.updateMissiontoJob(missionID, jobIDs);
+//				} else if (resp == 0) {
+//					missionDAO.deleteMissionRelatedLocation(missionID);
+//					missionDAO.deleteAllowedCountry(missionID);
+//					missionDAO.deleteMissionDAO(missionID);
+//					logger.error("Enter Detials is not correct  Job");
+//					result.put("status", "failed");
+//					result.put("responseMsg", "Enter Detials is not correct Job");
+//					return result;
+//				}
+//				
+//
+//				result.put("missionID ", missionID);
+//				result.put("status", "success");
+//				result.put("responseMsg", "successfully inserted");
+//			} else {
+//				logger.error("Enter Detials is not correct ");
+//				result.put("status", "failed");
+//				result.put("responseMsg", "Enter Details is not correct ");
+//			}
+//		} catch (
+//
+//		DuplicateKeyException e) {
+//			logger.error("Duplciate key error" + e);
+//			result.put("status", "failed");
+//			result.put("responseMsg", "Duplicate entry already present");
+//		} catch (UncategorizedSQLException e) {
+//			logger.error("Uncategorized  error" + e);
+//			result.put("status", "failed");
+//			result.put("responseMsg", "Enter data is not correct");
+//		}
+//					return result;
+		return null;
 	}
 
 	// Method for getting mission details using missionID or missionName
@@ -206,12 +221,13 @@ public class MissionServcie {
 		return result;
 	}
 
-	//Method for updating mission details using missionID or missionName
+	// Method for updating mission details using missionID or missionName
 	public Map<String, Serializable> updateMissionDetailsService(String missionID, Map<String, ?> props) {
 		logger.info("in updateMissionDetailsService ");
-		//updating mission details
+		// updating mission details
 		Map<String, Serializable> result = missionDAO.updatedetailsDAO(missionID, props);
-		
+
 		return result;
 	}
+
 }
